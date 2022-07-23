68cf53d7ec9269059e5ebc4d0e6c35bf2b034575
Hakim-Adjedjou <60810965+Hakim-Adjedjou@users.noreply.github.com>
2022-07-23 07:43:52 +0100

    modified

--- Stringr_manipulation.R
+++ Stringr_manipulation.R
@@ -26,3 +26,25 @@ employee_skills <- c("John Bale (Beginner)",
 
 res3<-str_match(employee_skills,"(.*)\\((Pro|Medium).*") %>% .[,c(2:3)] 
 colnames(res3)<-c("name","level")
+
+#########################################################################
+
+#web scrapping : 
+
+library(rvest)
+
+#choosing a topic from reddit : 
+
+df<-read_html("https://www.reddit.com/r/worldnews/comments/w55y29/were_losing_but_moscow_will_never_admit_it/")
+
+# returning the title tag : 
+df %>% html_node("h1._eYtD2XCVieq6emjKBH3m") %>% html_text()
+
+#returning all comments : 
+comments<-df %>% html_nodes("div._3cjCphgls6DH-irkVaA0GM ")%>% html_text()
+
+#returning time of the tweets : 
+time_comments<-df %>% html_nodes("a._3yx4Dn0W3Yunucf5sVJeFU")%>% html_text()
+
+#returning the names of those who wrote the tweets: 
+user_reddit<-df %>% html_nodes("div._3cjCphgls6DH-irkVaA0GM") %>% html_text()
